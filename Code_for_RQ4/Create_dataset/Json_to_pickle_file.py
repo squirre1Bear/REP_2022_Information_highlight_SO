77f3c0a5f6c84f7afeed1453b6482e2a025d6539
shahlashaan <37183177+shahlashaan@users.noreply.github.com>
2023-03-12 14:06:10 -0700

    Add files via upload

---
+++ Code_for_RQ4/Create_dataset/Json_to_pickle_file.py
@@ -0,0 +1,40 @@
+import logging
+import json
+import pickle
+from csv import reader,DictWriter
+from itertools import islice
+
+
+def main(input_file="/home/shahla/ProjectWorkHTML/Posts_csv_files/Dataset_files_other_formats/emphasis_data_ver1_train.json", output_file="/home/shahla/ProjectWorkHTML/Posts_csv_files/Dataset_files_other_formats/emphasis_data_ver1_train"):    
+    try:
+        training_data = []
+        lines=[]
+        with open(input_file, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            data = json.loads(line)
+            text = data['content']
+            entities = []
+            for annotation in data['annotation']:
+                point = annotation['points'][0]
+                labels = annotation['label']
+                if not isinstance(labels, list):
+                    labels = [labels]
+
+                for label in labels:
+                    entities.append((point['start'], point['end'] + 1 ,label))
+
+
+            training_data.append((text, {"entities" : entities}))
+
+        print(training_data)
+
+        with open(output_file, 'wb') as fp:
+            pickle.dump(training_data, fp)
+
+    except Exception as e:
+        logging.exception("Unable to process " + input_file + "\n" + "error = " + str(e))
+        return None
+if __name__ == '__main__':
+    main()
